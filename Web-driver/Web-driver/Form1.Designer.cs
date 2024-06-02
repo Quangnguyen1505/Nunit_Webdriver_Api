ff0d6d173841eea9050dba38a83e908e0921fec4
Thanh Quang <quang0706r@gmail.com>
2024-06-02 19:46:04 +0700

    #2 - Webdriver

---
+++ Web-driver/Web-driver/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Web_driver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtcn1 = new System.Windows.Forms.TextBox();
+            this.btncn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtcn2 = new System.Windows.Forms.TextBox();
+            this.txtcn3 = new System.Windows.Forms.TextBox();
+            this.txtcn4 = new System.Windows.Forms.TextBox();
+            this.btncn2 = new System.Windows.Forms.Button();
+            this.btncn3 = new System.Windows.Forms.Button();
+            this.btncn4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtcn1
+            // 
+            this.txtcn1.Location = new System.Drawing.Point(257, 139);
+            this.txtcn1.Name = "txtcn1";
+            this.txtcn1.Size = new System.Drawing.Size(174, 20);
+            this.txtcn1.TabIndex = 0;
+            // 
+            // btncn1
+            // 
+            this.btncn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncn1.Location = new System.Drawing.Point(464, 139);
+            this.btncn1.Name = "btncn1";
+            this.btncn1.Size = new System.Drawing.Size(75, 23);
+            this.btncn1.TabIndex = 1;
+            this.btncn1.Text = "Open";
+            this.btncn1.UseVisualStyleBackColor = true;
+            this.btncn1.Click += new System.EventHandler(this.btncn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(280, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Các Chức Năng Webdriver";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chức năng 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Chức năng 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Chức năng 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 287);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Chức năng 4";
+            // 
+            // txtcn2
+            // 
+            this.txtcn2.Location = new System.Drawing.Point(257, 197);
+            this.txtcn2.Name = "txtcn2";
+            this.txtcn2.Size = new System.Drawing.Size(174, 20);
+            this.txtcn2.TabIndex = 7;
+            // 
+            // txtcn3
+            // 
+            this.txtcn3.Location = new System.Drawing.Point(257, 241);
+            this.txtcn3.Name = "txtcn3";
+            this.txtcn3.Size = new System.Drawing.Size(174, 20);
+            this.txtcn3.TabIndex = 8;
+            // 
+            // txtcn4
+            // 
+            this.txtcn4.Location = new System.Drawing.Point(257, 287);
+            this.txtcn4.Name = "txtcn4";
+            this.txtcn4.Size = new System.Drawing.Size(174, 20);
+            this.txtcn4.TabIndex = 9;
+            // 
+            // btncn2
+            // 
+            this.btncn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncn2.Location = new System.Drawing.Point(464, 195);
+            this.btncn2.Name = "btncn2";
+            this.btncn2.Size = new System.Drawing.Size(75, 23);
+            this.btncn2.TabIndex = 10;
+            this.btncn2.Text = "Open";
+            this.btncn2.UseVisualStyleBackColor = true;
+            this.btncn2.Click += new System.EventHandler(this.btncn2_Click);
+            // 
+            // btncn3
+            // 
+            this.btncn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncn3.Location = new System.Drawing.Point(464, 238);
+            this.btncn3.Name = "btncn3";
+            this.btncn3.Size = new System.Drawing.Size(75, 23);
+            this.btncn3.TabIndex = 11;
+            this.btncn3.Text = "Open";
+            this.btncn3.UseVisualStyleBackColor = true;
+            this.btncn3.Click += new System.EventHandler(this.btncn3_Click);
+            // 
+            // btncn4
+            // 
+            this.btncn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncn4.Location = new System.Drawing.Point(464, 285);
+            this.btncn4.Name = "btncn4";
+            this.btncn4.Size = new System.Drawing.Size(75, 23);
+            this.btncn4.TabIndex = 12;
+            this.btncn4.Text = "Open";
+            this.btncn4.UseVisualStyleBackColor = true;
+            this.btncn4.Click += new System.EventHandler(this.btncn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btncn4);
+            this.Controls.Add(this.btncn3);
+            this.Controls.Add(this.btncn2);
+            this.Controls.Add(this.txtcn4);
+            this.Controls.Add(this.txtcn3);
+            this.Controls.Add(this.txtcn2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncn1);
+            this.Controls.Add(this.txtcn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtcn1;
+        private System.Windows.Forms.Button btncn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtcn2;
+        private System.Windows.Forms.TextBox txtcn3;
+        private System.Windows.Forms.TextBox txtcn4;
+        private System.Windows.Forms.Button btncn2;
+        private System.Windows.Forms.Button btncn3;
+        private System.Windows.Forms.Button btncn4;
+    }
+}
+
---
+++ Web-driver/Web-driver/Form1.cs
@@ -0,0 +1,170 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium.Support.UI;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
+
+namespace Web_driver
+{
+    public partial class Form1 : Form
+    {
+        private String url = "https://drawsql.app/";
+        private String urlcn = "https://opensource-demo.orangehrmlive.com/";
+        private String urlcn3 = "https://myshoes.vn/";
+        private String urlcn4 = "https://www.youtube.com/";
+        public Form1()
+        {
+            InitializeComponent();
+            txtcn1.Text = url;
+            txtcn2.Text = urlcn;
+            txtcn3.Text = urlcn3;
+            txtcn4.Text = urlcn4;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncn1_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(chrome);
+            driver.Url = url;
+            driver.Navigate();
+            System.Threading.Thread.Sleep(1000);
+            IWebElement button_submit = driver.FindElement(By.XPath("//a[@class='hidden lg:block font-medium text-gray-500 hover:text-gray-900 ml-6' and @href='/login']"));
+            button_submit.Click();
+            System.Threading.Thread.Sleep(1000);
+            IWebElement button_submit_register = driver.FindElement(By.CssSelector("a.ml-3.text-indigo-600.hover\\:text-indigo-500[href='https://drawsql.app/register']"));
+            button_submit_register.Click();
+
+            //input 
+            System.Threading.Thread.Sleep(1000);
+            IWebElement element_name = driver.FindElement(By.Id("name"));
+            element_name.SendKeys("Bui Thi Phuong Mai");
+
+            IWebElement element_email = driver.FindElement(By.Id("email"));
+            element_email.SendKeys("geqisodavefa@rungel.net\r\n");
+
+            IWebElement element_password = driver.FindElement(By.Id("password"));
+            element_password.SendKeys("Mai2003@");
+
+            System.Threading.Thread.Sleep(2000);
+            IWebElement btnSignUp = driver.FindElement(By.CssSelector("button.inline-flex.items-center.px-4.py-2.bg-indigo-500.border.border-transparent.rounded-md.font-medium.text-sm.text-white.tracking-wide.hover\\:bg-indigo-600.focus\\:outline-none.focus\\:ring-2.focus\\:ring-offset-2.focus\\:ring-offset-gray-50.focus\\:ring-indigo-500.disabled\\:opacity-25.transition.mt-8.w-full.justify-center"));
+            btnSignUp.Click();
+
+            System.Threading.Thread.Sleep(5000);
+            IWebElement element_textteam = driver.FindElement(By.Id("name"));
+            element_textteam.SendKeys("Team tester");
+
+            System.Threading.Thread.Sleep(2000);
+            IWebElement btnCreateTeam = driver.FindElement(By.XPath("//*[@id=\"app\"]/main/div[2]/div/div/div[2]/form/button"));
+            btnCreateTeam.Click();
+        }
+
+        private void btncn2_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(chrome);
+            driver.Url = urlcn3;
+            driver.Navigate();
+            System.Threading.Thread.Sleep(3000);
+
+            
+            IWebElement btnProduct = driver.FindElement(By.XPath("//*[@id=\"products-665afaa835b16-tab-1\"]/div/div[2]/div/div[2]"));
+            btnProduct.Click();
+            
+            IWebElement btnProductAdd = driver.FindElement(By.Id("button-cart"));
+            btnProductAdd.Click();
+
+            System.Threading.Thread.Sleep(5000);
+            IWebElement button_Cart = driver.FindElement(By.ClassName("btn-extra-46"));
+            button_Cart.Click();
+
+            System.Threading.Thread.Sleep(5000);
+            IWebElement button_quantity = driver.FindElement(By.ClassName("fa-angle-up"));
+            button_quantity.Click();
+        }
+
+        private void btncn3_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(chrome);
+            driver.Url = urlcn;
+            driver.Navigate();
+            System.Threading.Thread.Sleep(3000);
+
+
+            IWebElement element_username = driver.FindElement(By.Name("username"));
+            element_username.SendKeys("Admin");
+
+            IWebElement element_password = driver.FindElement(By.Name("password"));
+            element_password.SendKeys("admin123");
+
+            System.Threading.Thread.Sleep(1000);
+            IWebElement button_send_login = driver.FindElement(By.ClassName("orangehrm-login-button"));
+            button_send_login.Click();
+
+            System.Threading.Thread.Sleep(3000);
+            IWebElement button_submit_edit = driver.FindElement(By.XPath("//a[@class='oxd-main-menu-item' and @href='/web/index.php/pim/viewPimModule']"));
+            button_submit_edit.Click();
+
+            System.Threading.Thread.Sleep(3000);
+            IWebElement button_click_text = driver.FindElement(By.XPath("//button[contains(@class, 'oxd-button') and .//i[contains(@class, 'oxd-icon')]]"));
+            button_click_text.Click();
+
+            System.Threading.Thread.Sleep(1000);
+            IWebElement element_firstName = driver.FindElement(By.Name("firstName"));
+            element_firstName.SendKeys("Bui Thi Phuong Mai");
+
+            IWebElement element_middleName = driver.FindElement(By.Name("middleName"));
+            element_middleName.SendKeys("Thi Phuong");
+
+            IWebElement element_lastName = driver.FindElement(By.Name("lastName"));
+            element_lastName.SendKeys("Mai");
+
+            System.Threading.Thread.Sleep(3000);
+
+            IWebElement saveButton = driver.FindElement(By.CssSelector("button.oxd-button.oxd-button--medium.oxd-button--secondary.orangehrm-left-space"));
+
+            // Click the button
+            saveButton.Click();
+        }
+
+        private void btncn4_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(chrome);
+            driver.Url = urlcn4;
+            driver.Navigate();
+            System.Threading.Thread.Sleep(3000);
+
+            IWebElement btntextSearch = driver.FindElement(By.Name("search_query"));
+            btntextSearch.SendKeys("Tình đắng như ly cafe");
+
+            System.Threading.Thread.Sleep(2000);
+            IWebElement btnSearch = driver.FindElement(By.XPath("//*[@id=\"search-icon-legacy\"]/yt-icon/yt-icon-shape/icon-shape/div"));
+            btnSearch.Click();
+
+            System.Threading.Thread.Sleep(2000);
+            /*IWebElement video = driver.FindElement(By.XPath("//*[@id=\"dismissible\"]/div/div[3]/yt-formatted-string"));
+            video.Click();*/
+            IWebElement video = driver.FindElement(By.Id("dismissible"));
+            video.Click();
+        }
+    }
+}
